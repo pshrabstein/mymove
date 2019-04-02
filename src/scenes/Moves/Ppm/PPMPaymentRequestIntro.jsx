681a63ce3ce9f5f96896023c678d483c250dcb41
Ron Cruz <Ronolibert@gmail.com>
2019-04-02 15:55:21 +0000

    Add markup for ppm payment request intro

--- src/scenes/Moves/Ppm/PPMPaymentRequestIntro.jsx
+++ src/scenes/Moves/Ppm/PPMPaymentRequestIntro.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import PPMPaymentRequestActionBtns from './PPMPaymentRequestActionBtns';
 import './PPMPaymentRequest.css';
 
 const PPMPaymentRequestIntro = () => {
   return (
-    <div className="usa-grid">
+    <div className="usa-grid ppm-payment-req-intro">
+      <h3 className="title">Request PPM Payment</h3>
+      <p>You'll need the following documents</p>
+      <ul>
+        <li>
+          <strong>Weight tickets</strong> both empty & full, for each vehicle and trip
+        </li>
+        <li>
+          <strong>Storage and moving expenses</strong> (optional), such as:
+          <ul>
+            <li>storage</li>
+            <li>tolls & weighing fees</li>
+            <li>rental equipment</li>
+          </ul>
+        </li>
+      </ul>
+      <p>
+        <Link to="/allowable-expenses">List of allowable expenses</Link>
+      </p>
       {/* TODO: change onclick handler to go to next page in flow */}
       <PPMPaymentRequestActionBtns onClick={() => {}} nextBtnLabel="Get Started" />
     </div>
